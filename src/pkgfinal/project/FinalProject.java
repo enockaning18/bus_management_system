8c0a736b762043d7f35c6167f4f495aee3cdda22
_Okyere <enockaning18@gmailcom>
2024-03-03 17:00:05 +0000

    first commit

---
+++ src/pkgfinal/project/FinalProject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgfinal.project;
+
+/**
+ *
+ * @author okyere
+ */
+public class FinalProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        main_screen main_view = new main_screen();
+        main_view.setVisible(true);
+    }
+    
+}
